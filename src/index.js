b9d17cafcb8e03144f545bab24f1c0672894b563
Rob <robert.gentile@powerhrg.com>
2021-05-28 14:50:57 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => { 
+  let form = document.querySelector("#create-task-form")
+  let newTask = document.querySelector("#new-task-description")
+
+  const addNewItem = (event) => { 
+    event.preventDefault()
+    let element = document.createElement('li')
+    let list = document.querySelector('#tasks') 
+    list.append(element)
+    element.innerText = newTask.value 
+ }
+ form.addEventListener("submit" , addNewItem)
+
 });
